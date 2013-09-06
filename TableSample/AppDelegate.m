982b23a2225a716ce56ca5756887ea07c138e145
Nassar Stoertz <nassar@namomedia.com>
2013-09-05 17:50:04 -0700

    Add MIT license headers, and remove references to Namo

--- TableSample/AppDelegate.m
+++ TableSample/AppDelegate.m
@@ -1,12 +1,23 @@
-//
-//  AppDelegate.m
-//  TableSample
-//
-//  Created by Gabor Cselle on 9/2/13.
 //  Copyright (c) 2013 Namo Media. All rights reserved.
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
-#import <NamoAdsSDK/Namo.h>
 #import <Parse/Parse.h>
 
 #import "AppDelegate.h"
@@ -17,17 +28,12 @@
 - (BOOL) application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [Parse setApplicationId:@"U8ipUhuguVcq8zid2nIH1OU7jvwYuBi2G2QBzUqT"
                 clientKey:@"JYq1sRB1ZBh6AmD0UvGNVsLq5DwyIr5PnZikF1QN"];
-  [Namo setApplicationId:@"a1-6d85054334e6a247910"];
 
   self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-
   MainViewController *mainVC = [[MainViewController alloc] init];
-
   UINavigationController
       *navigationController = [[UINavigationController alloc] initWithRootViewController:mainVC];
-
   [self.window setRootViewController:navigationController];
-
   [self.window makeKeyAndVisible];
 
   return YES;
--- TableSample/MainViewController.m
+++ TableSample/MainViewController.m
@@ -1,11 +1,23 @@
-//
-//  MainViewController.m
-//  TableSample
-//
-//  Created by Gabor Cselle on 9/2/13.
 //  Copyright (c) 2013 Namo Media. All rights reserved.
 //
-#import <NamoAdsSDK/Namo.h>
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #import <Parse/Parse.h>
 
 #import "MainViewController.h"
@@ -14,8 +26,6 @@
 
 @interface MainViewController ()
 @property(nonatomic, strong) NSArray *content;
-@property(nonatomic, strong) NAMOTableViewAdPlacer *adPlacer;
-
 @end
 
 @implementation MainViewController
@@ -35,26 +45,21 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
 
-  self.adPlacer = [NAMOTableViewAdPlacer adPlacer];
-  [self.adPlacer bindToTableView:self.tableView];
-  [self.adPlacer setAdPlacementWithFirstPosition:2 spacing:3 maxAds:10];
   self.title = @"TableSample";
   
   [self.tableView registerClass:[ContentCell class]
          forCellReuseIdentifier:[ContentCell reuseIdentifier]];
-  [self.tableView namo_registerAdCellClass:[NAMOTableViewAdCellThumb class]];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
   [super viewWillAppear:animated];
-  [self.adPlacer requestAds];
   
   // load info from Parse
   PFQuery *query = [PFQuery queryWithClassName:@"BoardItem"];
   [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
     if (!error) {
       self.content = objects;
-      [self.tableView namo_reloadData];
+      [self.tableView reloadData];
     } else {
       // Log details of the failure
       NSLog(@"Error: %@ %@", error, [error userInfo]);
